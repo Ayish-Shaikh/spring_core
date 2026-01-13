c795052fdbf00a384329263f6fba2043e09afd45
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-01-13 08:36:35 +0530

    Notification Service comment line updated :>

--- src/main/java/TightCoupling/NotificationService.java
+++ src/main/java/TightCoupling/NotificationService.java
@@ -2,6 +2,7 @@ package TightCoupling;
 
 public class NotificationService {
     public void send(String message){
+        //SMS
         System.out.println("Notification received: " + message);
     }
 }
