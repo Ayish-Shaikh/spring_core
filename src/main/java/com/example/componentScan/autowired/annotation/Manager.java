2108f825af6850e1d5579d6a45875d78ff77522b
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-03-27 22:09:44 +0530

    qualifier is added cuz of handling multiple beans with the same name

--- src/main/java/com/example/componentScan/autowired/annotation/Manager.java
+++ src/main/java/com/example/componentScan/autowired/annotation/Manager.java
@@ -1,12 +1,14 @@
 package com.example.componentScan.autowired.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Manager {
     // FIELD INJECTION
     @Autowired
+    @Qualifier("employee")
     private Employee employee;
 
 
